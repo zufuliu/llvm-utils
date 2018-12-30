b1a6d43d7406a7a00927fc4dbf1de322faf76d11
zufuliu <zufuliu@gmail.com>
2018-12-30 17:45:12 +0800

    Fix typo.

--- VS2017/install.bat
+++ VS2017/install.bat
@@ -3,9 +3,9 @@ SETLOCAL ENABLEEXTENSIONS
 
 IF EXIST "%~1" (
 	SET "VS_PATH=%~1"
-	SET "EXIST_ON_ERROR=%~2"
+	SET "EXIT_ON_ERROR=%~2"
 ) ELSE (
-	SET "EXIST_ON_ERROR=%~1"
+	SET "EXIT_ON_ERROR=%~1"
 
 	FOR /f "delims=" %%A IN (
 	'"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -property installationPath'
@@ -18,7 +18,7 @@ IF EXIST "%~1" (
 SET VCT_PATH=%VS_PATH%\Common7\IDE\VC\VCTargets\Platforms
 IF NOT EXIST "%VCT_PATH%" (
 	ECHO Visual C++ 2017 or 2019 NOT Installed.
-	IF "%EXIST_ON_ERROR%" == "" PAUSE
+	IF "%EXIT_ON_ERROR%" == "" PAUSE
 	ENDLOCAL
 	EXIT /B
 )
@@ -34,7 +34,7 @@ XCOPY /Q /Y "LLVM_v141" "%VCT_PATH%\Win32\PlatformToolsets\LLVM_v141\"
 XCOPY /Q /Y "LLVM_v141_xp" "%VCT_PATH%\Win32\PlatformToolsets\LLVM_v141_xp\"
 XCOPY /Q /Y "LLVM_v141" "%VCT_PATH%\ARM64\PlatformToolsets\LLVM_v141\"
 
-IF "%EXIST_ON_ERROR%" == "" PAUSE
+IF "%EXIT_ON_ERROR%" == "" PAUSE
 POPD
 ENDLOCAL
-EXIST /B
+EXIT /B
