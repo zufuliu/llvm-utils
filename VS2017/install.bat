82ffdffad2f16fc32b6a9cab9e23159d8b796138
zufuliu <zufuliu@gmail.com>
2019-01-28 12:07:19 +0800

    Support Visual C++ 2017 v141 toolset for Visual Studio 2019.

--- VS2017/install.bat
+++ VS2017/install.bat
@@ -10,13 +10,16 @@ SET VSWHERE=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe
 @rem Visual Studio 2017
 FOR /f "delims=" %%A IN ('"%VSWHERE%" -property installationPath -prerelease -version [15.0^,16.0^)') DO (
 	SET VCT_PATH=%%A\Common7\IDE\VC\VCTargets\Platforms
-	IF EXIST "!VCT_PATH!" CALL :SUB_VS2017 "!VCT_PATH!"
+	IF EXIST "!VCT_PATH!" CALL :SUB_VS2017 "!VCT_PATH!" 2017
 )
 
 @rem Visual Studio 2019
 FOR /f "delims=" %%A IN ('"%VSWHERE%" -property installationPath -prerelease -version [16.0^,17.0^)') DO (
 	SET VCT_PATH=%%A\MSBuild\Microsoft\VC\v160\Platforms
 	IF EXIST "!VCT_PATH!" CALL :SUB_VS2019 "!VCT_PATH!"
+	@rem Visual C++ 2017 v141 toolset
+	SET VCT_PATH=%%A\MSBuild\Microsoft\VC\v150\Platforms
+	IF EXIST "!VCT_PATH!" CALL :SUB_VS2017 "!VCT_PATH!" 2019
 )
 
 IF %SUCCESS% == 0 (
@@ -30,13 +33,15 @@ EXIT /B
 
 
 :SUB_VS2017
-ECHO VCTargetsPath for Visual Studio 2017 or 2019 Preview 1: %~1
+ECHO VCTargetsPath for Visual Studio %~2: %~1
 XCOPY /Q /Y "LLVM" "%~1\..\LLVM\"
 XCOPY /Q /Y "LLVM_v141" "%~1\x64\PlatformToolsets\LLVM_v141\"
-XCOPY /Q /Y "LLVM_v141_xp" "%~1\x64\PlatformToolsets\LLVM_v141_xp\"
 XCOPY /Q /Y "LLVM_v141" "%~1\Win32\PlatformToolsets\LLVM_v141\"
-XCOPY /Q /Y "LLVM_v141_xp" "%~1\Win32\PlatformToolsets\LLVM_v141_xp\"
 XCOPY /Q /Y "LLVM_v141" "%~1\ARM64\PlatformToolsets\LLVM_v141\"
+IF /I "%~2" == "2017" (
+	XCOPY /Q /Y "LLVM_v141_xp" "%~1\Win32\PlatformToolsets\LLVM_v141_xp\"
+	XCOPY /Q /Y "LLVM_v141_xp" "%~1\x64\PlatformToolsets\LLVM_v141_xp\"
+)
 SET SUCCESS=1
 EXIT /B
 
