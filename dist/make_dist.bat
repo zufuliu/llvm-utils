fc9ba713b74d26e5550d5420516d2e47f07ea2af
zufuliu <zufuliu@gmail.com>
2019-01-28 20:59:48 +0800

    Include license.txt within zip archives (source distribution).

--- dist/make_dist.bat
+++ dist/make_dist.bat
@@ -8,6 +8,7 @@ CD /D %~dp0
 
 ECHO Visual Studio 2017 and 2019
 RD /Q /S LLVM_VS2017
+XCOPY /Q /Y ..\license.txt                 LLVM_VS2017\license.txt*
 XCOPY /Q /S /Y ..\VS2017                   LLVM_VS2017\
 IF "%MAKE_ZIP_ARCHIVE%" == "" CALL :SUB_MAKE_ZIP LLVM_VS2017
 IF "%KEEP_DIST_FOLDER%" == "" RD /Q /S LLVM_VS2017
@@ -15,6 +16,7 @@ IF "%KEEP_DIST_FOLDER%" == "" RD /Q /S LLVM_VS2017
 
 ECHO Visual Studio 2010 to 2015
 RD /Q /S LLVM_VS2010_2015
+XCOPY /Q /Y ..\license.txt                 LLVM_VS2010_2015\license.txt*
 XCOPY /Q /Y ..\VS2017\LLVM                 LLVM_VS2010_2015\LLVM\
 XCOPY /Q /S /Y ..\VS2015                   LLVM_VS2010_2015\
 XCOPY /Q /Y install_VS2010_2015.bat        LLVM_VS2010_2015\install.bat*
@@ -24,6 +26,7 @@ IF "%KEEP_DIST_FOLDER%" == "" RD /Q /S LLVM_VS2010_2015
 
 ECHO Visual Studio 2015
 RD /Q /S LLVM_VS2015
+XCOPY /Q /Y ..\license.txt              LLVM_VS2015\license.txt*
 XCOPY /Q /Y ..\VS2017\LLVM              LLVM_VS2015\LLVM\
 XCOPY /Q /Y ..\VS2015\LLVM_v140         LLVM_VS2015\LLVM_v140\
 XCOPY /Q /Y ..\VS2015\LLVM_v140_xp      LLVM_VS2015\LLVM_v140_xp\
@@ -34,6 +37,7 @@ IF "%KEEP_DIST_FOLDER%" == "" RD /Q /S LLVM_VS2015
 
 ECHO Visual Studio 2013
 RD /Q /S LLVM_VS2013
+XCOPY /Q /Y ..\license.txt              LLVM_VS2013\license.txt*
 XCOPY /Q /Y ..\VS2017\LLVM              LLVM_VS2013\LLVM\
 XCOPY /Q /Y ..\VS2015\LLVM_v120         LLVM_VS2013\LLVM_v120\
 XCOPY /Q /Y ..\VS2015\LLVM_v120_xp      LLVM_VS2013\LLVM_v120_xp\
@@ -44,6 +48,7 @@ IF "%KEEP_DIST_FOLDER%" == "" RD /Q /S LLVM_VS2013
 
 ECHO Visual Studio 2012
 RD /Q /S LLVM_VS2012
+XCOPY /Q /Y ..\license.txt                  LLVM_VS2012\license.txt*
 XCOPY /Q /Y ..\VS2017\LLVM                  LLVM_VS2012\LLVM\
 XCOPY /Q /Y ..\VS2015\x64\LLVM_v110         LLVM_VS2012\x64\LLVM_v110\
 XCOPY /Q /Y ..\VS2015\x64\LLVM_v110_xp      LLVM_VS2012\x64\LLVM_v110_xp\
@@ -56,6 +61,7 @@ IF "%KEEP_DIST_FOLDER%" == "" RD /Q /S LLVM_VS2012
 
 ECHO Visual Studio 2010
 RD /Q /S LLVM_VS2010
+XCOPY /Q /Y ..\license.txt                  LLVM_VS2010\license.txt*
 XCOPY /Q /Y ..\VS2017\LLVM                  LLVM_VS2010\LLVM\
 XCOPY /Q /Y ..\VS2015\x64\LLVM_v100         LLVM_VS2010\x64\LLVM_v100\
 XCOPY /Q /Y ..\VS2015\x64\LLVM_v90          LLVM_VS2010\x64\LLVM_v90\
