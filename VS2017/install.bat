3ee72c317650d73de7cf2ef241580b8b1a1b55ec
zufuliu <zufuliu@gmail.com>
2021-05-07 23:03:27 +0800

    Support Visual Studio Build Tools, issue #5.

--- VS2017/install.bat
+++ VS2017/install.bat
@@ -7,14 +7,30 @@ SET SUCCESS=0
 PUSHD %~dp0
 
 SET VSWHERE=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe
+
 @rem Visual Studio 2017
 FOR /f "delims=" %%A IN ('"%VSWHERE%" -property installationPath -prerelease -version [15.0^,16.0^)') DO (
 	SET VCT_PATH=%%A\Common7\IDE\VC\VCTargets\Platforms
 	IF EXIST "!VCT_PATH!" CALL :SUB_VS2017 "!VCT_PATH!" 2017
 )
 
+@rem Visual Studio 2017 Build Tools
+FOR /f "delims=" %%A IN ('"%VSWHERE%" -products Microsoft.VisualStudio.Product.BuildTools -property installationPath -prerelease -version [15.0^,16.0^)') DO (
+	SET VCT_PATH=%%A\Common7\IDE\VC\VCTargets\Platforms
+	IF EXIST "!VCT_PATH!" CALL :SUB_VS2017 "!VCT_PATH!" 2017
+)
+
 @rem Visual Studio 2019
 FOR /f "delims=" %%A IN ('"%VSWHERE%" -property installationPath -prerelease -version [16.0^,17.0^)') DO (
+	SET VCT_PATH=%%A\MSBuild\Microsoft\VC\v160\Platforms
+	IF EXIST "!VCT_PATH!" CALL :SUB_VS2019 "!VCT_PATH!"
+	@rem Visual C++ 2017 v141 toolset
+	SET VCT_PATH=%%A\MSBuild\Microsoft\VC\v150\Platforms
+	IF EXIST "!VCT_PATH!" CALL :SUB_VS2017 "!VCT_PATH!" 2019
+)
+
+@rem Visual Studio 2019 Build Tools
+FOR /f "delims=" %%A IN ('"%VSWHERE%" -products Microsoft.VisualStudio.Product.BuildTools -property installationPath -prerelease -version [16.0^,17.0^)') DO (
 	SET VCT_PATH=%%A\MSBuild\Microsoft\VC\v160\Platforms
 	IF EXIST "!VCT_PATH!" CALL :SUB_VS2019 "!VCT_PATH!"
 	@rem Visual C++ 2017 v141 toolset
