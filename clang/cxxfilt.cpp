f89fb6c0ce034e0e072585b7a10aa8dfaf81981b
zufuliu <zufuliu@gmail.com>
2022-10-09 06:18:21 +0800

    Add interactive mode for cxxfilt.

--- clang/cxxfilt.cpp
+++ clang/cxxfilt.cpp
@@ -3,12 +3,24 @@
 // clang-cl /std:c++20 /EHsc /O2 /GR- /GS- /MD /DNDEBUG /I../../include *.cpp
 
 #include "llvm/Demangle/Demangle.h"
+#include <cstdio>
+#include <iostream>
 
 int main(int argc, char *argv[]) {
-	for (int index = 1; index < argc; index++) {
-		const std::string name = argv[index];
-		const std::string result = llvm::demangle(name);
-		printf("%s\n    %s\n", name.c_str(), result.c_str());
+	if (argc > 1) {
+		for (int index = 1; index < argc; index++) {
+			const std::string name = argv[index];
+			const std::string result = llvm::demangle(name);
+			printf("%s\n    %s\n", name.c_str(), result.c_str());
+		}
+	} else {
+		std::string name;
+		while (true) {
+			printf(">> ");
+			std::getline(std::cin, name, '\n');
+			const std::string result = llvm::demangle(name);
+			printf("%s\n", result.c_str());
+		}
 	}
 	return 0;
 }
