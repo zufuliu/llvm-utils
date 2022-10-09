6a17ba96a1419064b4579552f4d9104d84ce4c2e
zufuliu <zufuliu@gmail.com>
2022-10-09 18:21:41 +0800

    Update cxxfilt to hanlde import thunk.

--- clang/cxxfilt.cpp
+++ clang/cxxfilt.cpp
@@ -6,20 +6,32 @@
 #include <cstdio>
 #include <iostream>
 
+static std::string demangle(const std::string &name) {
+	std::string result = llvm::demangle(name);
+	constexpr std::string_view prefix = "__imp_";
+	if (result == name && name.starts_with(prefix)) {
+		const std::string mangled = name.substr(prefix.length());
+		std::string undecorated = llvm::demangle(mangled);
+		if (undecorated != mangled) {
+			result = "import thunk for " + undecorated;
+		}
+	}
+	return result;
+}
+
 int main(int argc, char *argv[]) {
 	if (argc > 1) {
 		for (int index = 1; index < argc; index++) {
 			const std::string name = argv[index];
-			const std::string result = llvm::demangle(name);
+			const std::string result = demangle(name);
 			printf("%s\n    %s\n", name.c_str(), result.c_str());
 		}
 	} else {
+		printf(">>> ");
 		std::string name;
-		while (true) {
-			printf(">> ");
-			std::getline(std::cin, name, '\n');
-			const std::string result = llvm::demangle(name);
-			printf("%s\n", result.c_str());
+		while (std::getline(std::cin, name, '\n')) {
+			const std::string result = demangle(name);
+			printf("%s\n>>> ", result.c_str());
 		}
 	}
 	return 0;
